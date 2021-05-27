76f9ae2edf4e160fef208956e635dcc312f956ce
Matt McNaney <mcnaneym@appstate.edu>
2021-05-27 10:51:38 -0400

    Updated packages and webpack.

--- webpack.config.js
+++ webpack.config.js
@@ -1,33 +1,34 @@
-/* global __dirname */
+/* global module, __dirname */
 const path = require('path')
 module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: 'index.js',
-    libraryTarget: 'commonjs2'
+    libraryTarget: 'commonjs2',
   },
   resolve: {
     alias: {
-      'react': path.resolve(__dirname, './node_modules/react'),
+      react: path.resolve(__dirname, './node_modules/react'),
       'react-dom': path.resolve(__dirname, './node_modules/react-dom'),
-    }
+    },
   },
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'css-loader',
-        ],
-      }, {
+        use: [{loader: 'css-loader'}],
+      },
+      {
         test: /\.jsx?/,
         include: path.resolve(__dirname, 'src'),
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env', '@babel/preset-react',]
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
         },
       },
-    ]
-  }
+    ],
+  },
 }
